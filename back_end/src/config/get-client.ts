1e963d9612caba3d985923c7513ae94c2e8728b7
Tom <tomlambert1997@gmail.com>
2024-11-04 15:18:10 +0000

    added url database connection when not local

--- back_end/src/config/get-client.ts
+++ back_end/src/config/get-client.ts
@@ -1,16 +1,22 @@
-const { Client } = require("pg");
-require("dotenv").config();
+const { Client } = require('pg');
+require('dotenv').config();
 
 module.exports.getClient = async () => {
-  var client;
+  var client
 
-  client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  });
+  if (process.env.PG_HOST === 'localhost') {
+    client = new Client({
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+    })
+  } else {
+    client = new Client({
+      connectionString: process.env.DATABASE_URL + '?sslmode=require'
+    });
+  }
 
   await client.connect();
   return client;
