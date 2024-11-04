73c63261bbd422274d81886808e300590a58d442
Tom <tomlambert1997@gmail.com>
2024-11-04 15:11:01 +0000

    removed url database connection option

--- back_end/src/config/get-client.ts
+++ back_end/src/config/get-client.ts
@@ -1,22 +1,16 @@
-const { Client } = require('pg');
-require('dotenv').config();
+const { Client } = require("pg");
+require("dotenv").config();
 
 module.exports.getClient = async () => {
-  var client
-  var client
-  if (process.env.PG_HOST === 'localhost') {
-    client = new Client({
-      host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-    })
-  } else {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL + '?sslmode=require'
-    });
-  }
+  var client;
+
+  client = new Client({
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+  });
 
   await client.connect();
   return client;
